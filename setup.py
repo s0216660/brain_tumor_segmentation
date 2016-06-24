42fe152f03580726a8ad856cbd8618dd576f015d
Esther Alberts <esther.alberts@tum.de>
2016-06-24 13:55:57 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 # setup.
 setup(name='brain_tumor_em',
-      version='0.1',            # version is kind of arbitrary.
+      version='0.2',
       description='Expectation-Maximisation algoirthm to segment '
         'brain tumors in MR scans',
       author='Esther Alberts',
